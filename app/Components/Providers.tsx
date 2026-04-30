d17842f41d5b1135762f112519cc141a19a5b8ad
Mohd Ayaan Siddiqui <zxayaan2.0@gmail.com>
2026-04-30 14:18:34 +0530

    FIX: Remove unused and broken NextAuth provider

--- app/Components/Providers.tsx
+++ app/Components/Providers.tsx
@@ -1,11 +1,10 @@
 "use client"
 import { HeroUIProvider } from "@heroui/react"
-import { SessionProvider } from "next-auth/react"
 
 export default function Provider({ children }: { children: React.ReactNode }) {
     return (
         <HeroUIProvider>
-            <SessionProvider>{children}</SessionProvider>
+            {children}
         </HeroUIProvider>
     )
 }
